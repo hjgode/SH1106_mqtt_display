9190c6702288a1f7b06987b8882f1ca4c284b024
hjgode <hjgode@gmail.com>
2026-07-17 06:20:08 +0200

    changes to make it compile wih Arduino 1.8.13 and current libraries

--- SH1106_mqtt_display.ino
+++ SH1106_mqtt_display.ino
@@ -24,7 +24,120 @@
  *
  */
 
-#define PIN_POWER D5
+/* 
+ *  
+ Libraries
+
+  Using library ESP8266WiFi at version 1.0 in folder: .. packages/esp8266/hardware/esp8266/2.6.2/libraries/ESP8266WiFi 
+  Using library ESP8266mDNS at version 1.2 in folder: .. packages/esp8266/hardware/esp8266/2.6.2/libraries/ESP8266mDNS 
+  Using library pubsubclient-master at version 2.8 in folder: .. libraries/pubsubclient-master 
+  Using library Wire at version 1.0 in folder: .. /packages/esp8266/hardware/esp8266/2.6.2/libraries/Wire 
+  Using library ESP8266_and_ESP32_OLED_driver_for_SSD1306_displays at version 4.6.2 in folder: .. libraries/ESP8266_and_ESP32_OLED_driver_for_SSD1306_displays 
+
+
+ *  For programming other ESP8266 modules such as ESP-12 or ESP-07, wire the pins like these:
+
+For ESP8266
+flasher pin   ESP8266 pin
+VCC           VCC
+GND           GND
+TXD           RXD
+RXD           TXD
+RST           RST
+GPIO0         GPIO0
+CH_EN         CH_EN or CH_PD
+ *  
+  ESP12E Pins
+  GPIO Pins
+
+There is a total of 11 I/O pins. Therefore, these pins can perform multiple kinds of input and output functions that are interfaceable with all the TTL/CMOS devices. All GPIO pins are:
+
+    Pin4 – GPIO16
+    Pin5 – GPIO14
+    Pin6 – GPIO12
+    Pin7 – GPIO13
+    Pin11 – GPIO9
+    Pin12 – GPIO10
+    Pin16 – GPIO15
+    Pin18 – GPIO0
+    Pin19 – GPIO4
+    Pin20 – GPIO5
+
+UART Communication Pins
+
+The devices ESP 12E is programmable with UART communication in many IDE. Those UART pins are usable after even programming. Both pins are:
+
+    Pin 21 – RXD0
+    Pin 22 – TXD0
+    Pin15 – RTS
+    Pin13 – CTS
+    Pin2 – TXD
+
+ESP12E SPI Communication Pins
+
+The module offers direct SPI communication with itself. This communication uses four pins for proper communication, which are:
+
+    Pin12 – CS0
+    Pin10 – MISO
+    Pin9 – MOSI
+    Pin14 – SCLK
+
+I2C
+
+In ESP12E some GPIO pins are useable for I2C communication. In this communication, only two pins help to communicate. One for clock and other for data. Therefore, in ESP, both are:
+
+    Pin14 – SCL
+    Pin2 – SDA
+
+I2S
+
+It isn’t common in every device. The I2S helps to collect, process and transmit the audio signal. The I2S uses 6 pins and in ESP 12E all I2S pins are:
+
+    Pin12 – I2SI_DATA
+    Pin13 – I2SI_BCK
+    Pin14 – I2SI_WS
+    Pin15 – I2SO_BCK
+    Pin3 – I2SO_DATA
+    Pin2 – I2SO_WS
+
+PWM
+
+In every controller, the ESP 12E can convert it every pin into PWM pins through programming. There are three specifics for the PWM signal. All these pins are:
+
+    Pin12
+    Pin13
+    Pin15
+
+IR Interface
+
+The IR interface uses modulation and demodulation which requires NEC coding. The ESP12E is interfaceable using the IR interface pins and it is specific to these pins:
+
+    Pin5 – IR_R
+    Pin14 – IR_T
+
+ADC
+
+The device has a 10-bit ADC pin, which uses 0-1V range to convert the analog data to digital data.
+
+    Pin2
+
+Power and Control Pins
+
+Power : ESP12E uses the VCC pin to power up the whole module and the ground pin to make the common ground with power supply and other devices
+
+    Pin8 – VCC
+    Pin15 – GND
+
+Enable: The devices have an internal digital switch. The enable pin will receive the digital HIGH signal to activate the chip.
+
+    Pin3
+
+Reset: To reset the device the digital will help to reset it through the LOW state. The reset pin is the first pin of the module is:
+
+    Pin1
+*/
+
+#define PIN_POWER (5)
 long previousMillis = 0; 
 long interval = 1000;           // interval after which to shut down (milliseconds)
 long shutdown_counter=20;
@@ -84,15 +197,23 @@ PubSubClient client(espClient);
 // SH1106Spi         display(D0, D2);
 
 // Initialize the OLED display using brzo_i2c
-// D3 -> SDA
-// D5 -> SCL
+// D3 -> SDA    Pin2
+// D5 -> SCL    Pin14
 // SSD1306Brzo display(0x3c, D3, D5);
 // or
 // SH1106Brzo  display(0x3c, D3, D5);
 
 // Initialize the OLED display using Wire library
 //SSD1306  display(0x3c, D3, D5);
- SH1106 display(0x3c, D2, D1);
+
+
+// packages/esp8266/hardware/esp8266/2.6.2/variants/generic/pins_arduino.h
+// #define PIN_WIRE_SDA (4)
+// #define PIN_WIRE_SCL (5)
+// static const uint8_t SDA = PIN_WIRE_SDA;
+// static const uint8_t SCL = PIN_WIRE_SCL;
+
+ SH1106 display(0x3c, SDA, SCL);
 
 OLEDDisplayUi ui     ( &display );
 
@@ -289,8 +410,8 @@ void setup() {
   pinMode(PIN_POWER, OUTPUT);
   digitalWrite(PIN_POWER, HIGH);
   
-  Serial.begin(115200);
-  Serial.println();
+  Serial.begin(9600);
+  Serial.println('Hello');
   Serial.println();
 
   myText1 = "Aussen 11";
